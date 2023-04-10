b79c7dcd93f425a46f94484ad912bc19b7e43e38
Jose Pereira <jpereira.freelas@gmail.com>
2023-04-09 23:24:27 -0300

    desafio02

---
+++ docker/desafios/02_desafio_nginx_node/node/app/src/index.js
@@ -0,0 +1,46 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const config = {
+	host: 'db',
+	user: 'root',
+	password: 'root',
+	database: 'desafio02_db'
+}
+
+const mysql = require('mysql')
+const connection = mysql.createConnection(config)
+
+const createTable = `CREATE TABLE IF NOT EXISTS people(id int not null auto_increment, name varchar(255), primary key(id))`
+connection.query(createTable)
+
+const insert = `INSERT INTO people(name) VALUES ('Jose Pereira')`
+connection.query(insert)
+
+let namesResult = []
+
+const select = `SELECT * FROM people`
+connection.query(select, (err, result, fields) => {
+	namesResult = result
+})
+
+connection.end()
+
+app.get('/', (req, res) => {
+	let namesList = '<ol>'
+	
+	namesResult.forEach( (item) => {
+		namesList += `<li>${item.name}</li>`
+	})
+	
+	namesList += '</ol>'
+	
+	const result = `<h1>Full Cycle Rocks!</h1></br>${namesList}`
+
+	res.send(result)
+})
+
+app.listen(port, () => {
+	console.log('Rodando na porta ' + port)
+})
+
